2a202daba37a369c2201f57346c59a0f7fe0f53e
makerMcl <makerMcl@clauss.pro>
2021-04-05 12:16:29 +0200

    configurability of logging

--- hc12tool.h
+++ hc12tool.h
@@ -4,7 +4,8 @@
  * Simply connect the HC12 with your Txd0 and RxD0 pins, that is your default UART Arduino addresses with the Serial object:<ul>
  * <li> Txd0 of Arduino module  ==>  Rx pin of HC-12 module
  * <li> Rxd0 of Arduino module  ==>  Tx pin of HC-12 module
- * <li> 3.3V of Arduino module  ==> Vcc of HC-12 module
+ * <li> any output-able GPIO    ==>  set pin of HC-12 module
+ * <li> 3.3V of Arduino module  ==>  Vcc of HC-12 module
  * </ul>
  * 
  * That given any output/input on Serial is also transmitted/received via the HC-12 module and can be used to monitor the module from a remote location.
@@ -21,22 +22,24 @@
  * If not available, the remote module will receive the text:
  * <pre>Configuring HC-12: AT -> command mode not available</pre>
  * 
- * Optionally, sets more options of the HC-12 module:
- * <li>channel
- * <li>PA output
- * 
+ * Optionally, sets more options of the HC-12 module:<ul>
+ * <li>channel (AT+C)</li>
+ * <li>PA output (AT+P)</li>
+ * <li>transmission mode (AT+FU)</li>
+ * </ul>
  * 
  * Typical use (use heap instance since only needed in setup) might be:<pre>
  * #include "universalUIglobal.h"
  * void setup() {
- *   Hc12DebugTool hc12DebugTool(5);
- *   hc12DebugTool.setPreferredBaudrate(BPS115200);
+ *   Hc12Tool hc12Tool(5);
+ *   hc12Tool.setPreferredBaudrate(BPS115200);
  *   //...
  * }
  * </pre>
  * 
- * TODO/potential for more:
- * <li>support more configuration details, e.g. transmission power (AT+P), channel (AT+C), UART transmission mode (AT+FU)
+ * Per default, received unexpected bytes and activity info is logged to Serial. This can be configured by calling
+ * <pre>hc12Tool.setVerbosity();</pre>
+ * and affects subsequent actions.
  */
 #ifndef _HC12_DEBUG_TOOL_H
 #define _HC12_DEBUG_TOOL_H
@@ -68,6 +71,13 @@ enum Hc12_BaudRate
     BPS115200 = 7
 };
 
+struct Hc12toolVerbosity
+{
+    bool showUnexpectedBytes : 1;
+    bool printActityInfo : 1, : 5;
+    bool moduleConnected : 1;
+};
+
 // ordering must adhere to order of enum
 #define NUM_HC12_BAUDRATES 8
 const char *const HC12_BAUDRATE_STRING[NUM_HC12_BAUDRATES] /*PROGMEM*/ = {"1200", "2400", "4800", "9600", "19200", "38400", "57600", "115200"};
@@ -78,7 +88,7 @@ const char *const COMMAND_AT /*PROGMEM*/ = "AT\r\n";  // AT-command must be line
 const char *const RESPONSE_AT /*PROGMEM*/ = "OK\r\n"; // HC-12 terminates his responses always with \r\n (<13>,<10>)
 
 template <typename S>
-class Hc12DebugTool
+class Hc12Tool
 {
 public:
     /**
@@ -90,22 +100,20 @@ public:
      * If fallback is set to value 0, no fallback is performed.
      * 
      * Some notes on usage:<ul>
-     * <li>hc12debugTool tries to detect the current baudrate of the hc-12 module and will change if necessary.</li>
+     * <li>Hc12Tool tries to detect the current baudrate of the hc-12 module and will change if necessary.</li>
      * <li>It is not necessary to call begin() beforehand on the hc12Serial interface.
-     * hc12DebugTool detects that and will will try to dete</li>
+     * Hc12Tool detects that and will will try to dete</li>
      * 
      * @param setPin number of gpio the set pin of hc-12 module is/might be connected to.
      * @param hc12Serial UART interface (stack instance) the hc-12 is connected to
-     * @param debug Printer where activity info and unexpected bytes are written to
      * @param fallbackSerialTo if value>0 and command mode is not available, Serial baudrate is set to that value. Defaults value is 9600 [Baud].
      * @param waitForAvailableWrite set to 0 if using SoftwareSerial! value >0 is number of milliseconds to wait for Serial.availableForWrite()
      */
     // Note: one might need a mode without printing anything to debug... implement it if-needed using an additional flag and set-method
-    Hc12DebugTool(uint8_t setPin,
-                  S &hc12Serial,
-                  Print &debug,
-                  uint32_t fallbackSerialTo = 9600,
-                  uint8_t waitForAvailableWrite = 0) : _setPinNo(setPin), _hc12Serial(hc12Serial), _debug(debug), _fallbackSerialTo(fallbackSerialTo), _waitForAvailableWrite(waitForAvailableWrite) {}
+    Hc12Tool(uint8_t setPin,
+             S &hc12Serial,
+             uint32_t fallbackSerialTo = 9600,
+             uint8_t waitForAvailableWrite = 0) : _setPinNo(setPin), _hc12Serial(hc12Serial), _fallbackSerialTo(fallbackSerialTo), _waitForAvailableWrite(waitForAvailableWrite) {}
 
     /**
      * Sets the given baudrate for the module.
@@ -120,7 +128,7 @@ public:
             return;
         // read any bytes still in buffer/transmission
         dumpPendingBytes();
-        _debug.print(F("\nConfiguring HC-12: "));
+        logActivity(F("\nConfiguring HC-12: "));
         pinMode(_setPinNo, OUTPUT);
         digitalWrite(_setPinNo, LOW);
         delay(40); // wait to enter command mode
@@ -137,7 +145,7 @@ public:
             changeBaudRate(HC12_BAUDRATE_NUMERIC[baudRate]);
             if (sendValidatedCommand(COMMAND_AT, RESPONSE_AT, true))
             {
-                _debug.print(F(" hc12serial found at preferred baudrate, "));
+                logActivity(F(" hc12serial found at preferred baudrate, "));
                 commandModeEnabled = true;
             }
             else
@@ -146,7 +154,7 @@ public:
                 changeBaudRate(9600);
                 if (sendValidatedCommand(COMMAND_AT, RESPONSE_AT, true))
                 {
-                    _debug.print(F(" hc12serial found at 9600 baud, "));
+                    logActivity(F(" hc12serial found at 9600 baud, "));
                     commandModeEnabled = true;
                 }
                 else
@@ -159,9 +167,8 @@ public:
                         dumpPendingBytes();
                         if (sendValidatedCommand(COMMAND_AT, RESPONSE_AT, false))
                         {
-                            _debug.print(F(" found hc12serial at "));
-                            _debug.print(HC12_BAUDRATE_STRING[i]);
-                            _debug.print(F(" baud, "));
+                            logActivity(F(" found hc12serial at "), HC12_BAUDRATE_STRING[i]);
+                            logActivity(F(" baud, "));
                             commandModeEnabled = true;
                             break;
                         }
@@ -171,11 +178,12 @@ public:
         // tolerate incoming, unexpected bytes, because there could be more on the line
         if (commandModeEnabled) // assume set pin is connected since connected module reacts to AT commands,
         {
+            _verbosity.moduleConnected = true;
             // "AT+RB" - query baudrate: response should be: "OK+B9600"
             _hc12Serial.write("AT+RB", 5);
             if (readExpectedResponse("OK+B", false) && readExpectedResponse(HC12_BAUDRATE_STRING[baudRate], false))
             {
-                _debug.println(F("preferred baudrate already configured"));
+                logActivity(F("preferred baudrate already configured"));
             }
             else
             {
@@ -185,43 +193,66 @@ public:
                 {
                     if (readExpectedResponse(HC12_BAUDRATE_STRING[baudRate], false))
                     {
-                        _debug.print(F("baudrate set to "));
-                        _debug.println(HC12_BAUDRATE_STRING[baudRate]);
+                        logActivity(F("baudrate set to "), HC12_BAUDRATE_STRING[baudRate]);
                         changeBaudRate(HC12_BAUDRATE_NUMERIC[baudRate]);
                     }
                     else
                     {
                         if (0 < _fallbackSerialTo)
                         {
-                            _debug.println(F("unexpected response setting baudrate, setting local to fallback"));
+                            logActivity(F("unexpected response setting baudrate, setting local to fallback"));
                             changeBaudRate(_fallbackSerialTo);
                         }
                         else
-                            _debug.println(F("unexpected response setting baudrate"));
+                            logActivity(F("unexpected response setting baudrate"));
                     }
                 }
             }
         }
         else
         {
+            _verbosity.moduleConnected = false;
             if (0 < _fallbackSerialTo)
             {
-                _debug.println(F(" -> command mode not available, setting local to fallback"));
+                logActivity(F(" -> command mode not available, setting local to fallback"));
                 changeBaudRate(_fallbackSerialTo);
             }
             else
-                _debug.println(F(" -> command mode not available"));
+                logActivity(F(" -> command mode not available"));
         }
         // wait to enter UART mode
         delay(80);
     }
 
+    void set
+
+
+    /**
+     * Configures log verbosity.
+     * 
+     * @param showUnexpectedBytes if true, incoming unexpected bytes are dumped to debugStream
+     * @param showActivityInfo if true, messages for activity are printed to debugStream
+     * @param debugStream where to print verbose log to. Defaults to Serial
+     */
+    void setVerbosity(const bool showUnexpectedBytes, const bool showActivityInfo, Print &debugStream = Serial)
+    {
+        _verbosity.showUnexpectedBytes = showUnexpectedBytes;
+        _verbosity.printActityInfo = showActivityInfo;
+        _debug = debugStream;
+    }
+
+    void check() {
+        _debug.print("connected=");
+        _debug.println(_verbosity.moduleConnected);
+    }
+
 private:
     uint8_t _setPinNo;
     S &_hc12Serial;
-    Print &_debug;
     uint32_t _fallbackSerialTo;
     uint8_t _waitForAvailableWrite;
+    Hc12toolVerbosity _verbosity = {true, true, false};
+    Print &_debug = Serial;
 
     /** send command and wait for specific response */
     boolean sendValidatedCommand(const char *command, const char *expectedResponse, const bool tolerateUnexpected)
@@ -236,7 +267,7 @@ private:
             }
             if (!writeCycles)
             {
-                _debug.println(F("hc12serial not available for write"));
+                logActivity(F("hc12serial not available for write"));
                 return false;
             }
         }
@@ -280,7 +311,9 @@ private:
                 }
                 else
                 { // got something else
-                    _debug.write((char)x);
+                    if (_verbosity.showUnexpectedBytes)
+                        _debug.write((char)x);
+
                     if (tolerateUnexpected)
                     {
                         responsePos = 0;
@@ -299,19 +332,42 @@ private:
     {
         _hc12Serial.flush(); // always write pending data in TX-FIFO
         _hc12Serial.SETBAUDRATE(baudRate);
-        _debug.print(F("\nset baudrate to "));
+        logActivity(F("\nset baudrate to "));
         _debug.println(baudRate);
     }
 
     void dumpPendingBytes()
     {
-        if (_hc12Serial.available())
+        if (_verbosity.showUnexpectedBytes)
         {
-            _debug.print(F("<unexpected>"));
+            if (_hc12Serial.available())
+            {
+                _debug.print(F("<unexpected>"));
+                while (_hc12Serial.available())
+                    _debug.write(_hc12Serial.read());
+                _debug.print(F("</unexpected>"));
+                _debug.println();
+            }
+        }
+        else
+        {
             while (_hc12Serial.available())
-                _debug.write(_hc12Serial.read());
-            _debug.print(F("</unexpected>"));
-            _debug.println();
+                _hc12Serial.read();
+        }
+    }
+    void logActivity(const __FlashStringHelper *msg)
+    {
+        if (_verbosity.printActityInfo)
+        {
+            _debug.print(msg);
+        }
+    }
+    void logActivity(const __FlashStringHelper *msg, const char *value)
+    {
+        if (_verbosity.printActityInfo)
+        {
+            _debug.print(msg);
+            _debug.print(value);
         }
     }
 };
